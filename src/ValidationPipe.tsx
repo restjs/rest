9c41f3d59f6fc805b4701306bb01d1622e87c0b8
Alireza Roosta <alireza.roosta.1998@gmail.com>
2020-12-19 21:41:50 +0330

    class-transformer added to the ValidationPipe

--- src/ValidationPipe.tsx
+++ src/ValidationPipe.tsx
@@ -1,33 +1,32 @@
-import {validate} from "class-validator";
+import {validate, ValidatorOptions as ClassValidatorOptions} from "class-validator";
+import {ClassTransformOptions, plainToClass} from "class-transformer";
 import {HttpException} from "./HttpException";
 
-export interface ValidatorOptions {
+export interface ValidatorOptions extends ClassValidatorOptions{
     query ? : any;
     body ? : any;
     params ? : any;
-    skipMissingProperties?: boolean;
-    whitelist?: boolean;
-    forbidNonWhitelisted?: boolean;
-    groups?: string[];
-    dismissDefaultMessages?: boolean;
-    validationError?: {
-        target?: boolean;
-        value?: boolean;
-    };
-
-    forbidUnknownValues?: boolean;
+    transform? : boolean | ClassTransformOptions
 }
 
 export default class ValidationPipe {
-    params;
+    params : ValidatorOptions;
     async use(req){
-        const {body,query,params,...options} = this.params;
-        for(const param in this.params){
-            if(param == "body" || param == "query" || param == "params"){
-                const dto = new this.params[param];
-                if(Object.keys(req[param]).length > 0){
-                    for(const field in req[param]){
-                        dto[field] = req[param][field];
+        const {body,query,params,transform, ...options} = this.params;
+        for(const prop in this.params){
+            if(prop == "body" || prop == "query" || prop == "params"){
+                let dto ;
+                if(transform){
+                    let transformOptions = {};
+                    if(typeof transform == "object")
+                        transformOptions = transform;
+                    //@ts-ignore
+                    dto = plainToClass(this.params[prop], req[prop], transformOptions);
+                    req[prop] = dto;
+                }else if(Object.keys(req[prop]).length > 0){
+                    dto = new this.params[prop];
+                    for(const field in req[prop]){
+                        dto[field] = req[prop][field];
                     }
                 }
                 const errors = await validate(dto, options);
